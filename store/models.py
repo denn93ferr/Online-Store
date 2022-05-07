50254dd5040e8c4dc425a0bb55d24d7cc237dc69
Gledi Caushaj <gledi.alb@gmail.com>
2022-05-07 10:05:30 +0200

    Add linter & formatter

--- store/models.py
+++ store/models.py
@@ -1,38 +1,34 @@
 from django.db import models
-# from django.db.models import Model
 
-# Create your models here.
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(
-        verbose_name="Description of the product",
-        null = False,
-        blank = False
+        verbose_name="Description of the product", null=False, blank=False
     )
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    
-    
+
     class Meta:
         db_table = "products"
-    
 
 
-class Order (models.Model):
-    ORDER_STATUS_PENDING = 'P'
-    ORDER_STATUS_COMPLETE = 'C'
-    ORDER_STATUS_FAILED = 'F'
+class Order(models.Model):
+    ORDER_STATUS_PENDING = "P"
+    ORDER_STATUS_COMPLETE = "C"
+    ORDER_STATUS_FAILED = "F"
     ORDER_STATUS_CHOICES = [
-        (ORDER_STATUS_PENDING, 'Pending'),
-        (ORDER_STATUS_COMPLETE, 'Complete'),
-        (ORDER_STATUS_FAILED, 'Failed')
+        (ORDER_STATUS_PENDING, "Pending"),
+        (ORDER_STATUS_COMPLETE, "Complete"),
+        (ORDER_STATUS_FAILED, "Failed"),
     ]
     user_name = models.CharField(max_length=255, unique=True)
     total_cost = models.DecimalField(max_digits=6, decimal_places=2)
     date_of_submission = models.DateTimeField(auto_now_add=True)
-    order_status = models.CharField(max_length=1, choices=ORDER_STATUS_CHOICES, default=ORDER_STATUS_PENDING)
-    
+    order_status = models.CharField(
+        max_length=1, choices=ORDER_STATUS_CHOICES, default=ORDER_STATUS_PENDING
+    )
+
     products = models.ManyToManyField(Product)
-    
-    
+
     class Meta:
         db_table = "orders"
--- store/views.py
+++ store/views.py
@@ -1,10 +1,7 @@
-from ast import List
-from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Product
 
-# Create your views here.
 
 class ListProducts(ListView):
     model = Product
-    template_name = 'list_products.html'
+    template_name = "list_products.html"
