af96dceaec40483f28129587c7d444a283f142fe
Jur1 <jurgen95@live.com>
2022-05-20 14:46:31 +0200

    Add block content to login.html

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,11 +4,11 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class SignUpForm(UserCreationForm):
-    
     class Meta:
         model = User
-        fields = ('username', 'password1', 'password2')
-        
-    def save(self, commit= True):
+        fields = ("username", "password1", "password2")
+
+    def save(self, commit=True):
         user = super().save(commit=True)
